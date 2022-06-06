c89072e69045036f36974a3c15a4e823d814225b
abolfazl ezy <abolfazlandalib@gmail.com>
2022-06-06 01:08:37 -0800

    complete the first release of API

--- colab2/settings.py
+++ colab2/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import dotenv
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m*_l03xe6ktwkb_cp^xzjj+^4l89-l-)q3xbvc7(2qpg-*u1h('
+SECRET_KEY = str(os.environ.get("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,21 +9,39 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 import pandas as pd
 import os
+
+
 class GetCompanies(APIView):
+
     def get(self, format = None):
 
         df = pd.read_csv("core/data/company_name.csv")
+
         return Response(df['companyName'])
 
 
 
 class GetCompanyESG(APIView):
-    def get(self,format = None):
-        ric_code = self.request.GET.get('ric_code')
-        df = pd.read_csv("core/data/company_name.csv")
-        my_df = None
-        if ric_code not in df['ricCode'].values:
 
-            return Response('not found')
-        c_name = df[df.ricCode == ric_code].companyName
+    def get(self,format = None):
+
+        ric_code = self.request.GET.get('ric_code')
+
+        df = pd.read_csv("core/data/ESG.csv")
+
+        if ric_code not in df['code'].values:
+
+            return Response({'error':'not found'}, 404)
+
+        company_scores = pd.DataFrame(df[df.code == ric_code])
+
+        data_dict = {'code':company_scores['code'],
+                 'env':company_scores['enviromnent'],
+                 'goverance':company_scores['goverance'],
+                 'rank':company_scores['rank'],
+                 'social':company_scores['social']}
+
+        print(data_dict)
+        return Response(data_dict, status=200)
+
 
